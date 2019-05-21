2324298c582a76ba297ba388b5266321e82be4ea
alexpaxom <WorkAlexAn@gmail.com>
2019-05-21 14:48:52 +0300

    Классы для проверки вида токена. Каждый класс получает по одному символу через метод getToken и сохраняет его во внуренный буффер. До тех пор пока последовательность соответствует определенному виду токена.
    OperationsLexToken проверяет является ли токен одной из операций.
    NumberLexToken проверяет является ли токен числом.
    NumberLexToken проверяет является ли токен идентификатором.
    NumberLexToken проверяет является ли токен каким либо видом скобок.

---
+++ src/com/company/Lexer/OperationsLexToken.java
@@ -0,0 +1,41 @@
+package com.company.Lexer;
+
+public class OperationsLexToken implements LexToken
+{
+	String buffer = "";
+	
+	public String getToken()
+	{
+		return buffer;
+	}
+	
+	public String getType()
+	{
+		return "operation";
+	}
+	
+	public void restart()
+	{
+		buffer = "";
+	}
+	
+	public boolean addToToken(char simbol)
+	{
+		if(buffer.length() < 1)
+			switch(simbol)
+			{
+				case '+':
+				case '-':
+				case '*':
+				case '/':
+				case '%':
+				//case '>':
+				//case '<':
+				//case '=':
+					buffer = buffer + simbol;
+					return true;
+			}
+		
+		return false;
+	}
+}
